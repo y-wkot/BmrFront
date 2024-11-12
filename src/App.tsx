52e783b8b1672b77fda699b4ad6b9c53bd52191a
ukkey <y.spada1017@gmail.com>
2024-11-12 23:43:49 +0900

    241112f9

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import MainLayout from "./MainLayout";
+import ScrollToTop from "./ScrollToTop";
 import Settings from "./Settings";
 import TopPage from "./TopPage";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop /> {/* ページ遷移時にスクロールをリセット */}
       <Routes>
         <Route path="/" element={<TopPage />} />
         <Route path="/calculate" element={<MainLayout />} />
